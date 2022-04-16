27196f1943914858e3ba617cf2348a9e6f31e11a
JaeYoung0 <hjy8697@gmail.com>
2022-04-16 18:45:25 +0900

    fix: Wrapper

---
+++ src/app.style.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 500px;
+  .MuiDataGrid-columnHeaders {
+    background: #636378;
+  }
+
+  .MuiDataGrid-columnHeaderTitleContainer {
+    margin: 0 auto;
+  }
+
+  .MuiDataGrid-columnHeaderTitle {
+    color: #fff;
+  }
+
+  .MuiDataGrid-sortIcon {
+    color: #fff;
+  }
+`;
