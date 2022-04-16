dfb26dbf61bba4a5bf8bc19627aa78864d3823a9
JaeYoung0 <hjy8697@gmail.com>
2022-04-16 18:45:05 +0900

    feat: preact/compat

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     preact({
@@ -16,4 +15,10 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom": "preact/compat",
+    },
+  },
 });
