44012f75398e784619f713797e55d0fb3c87cc07
JaeYoung0 <hjy8697@gmail.com>
2022-04-30 22:34:10 +0900

    feat: swrConfig

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import Table from '@/components/Table'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { OptionsProvider } from '@/hooks/useOptions'
-import { Global, css } from '@emotion/react'
+import { SWRConfig } from 'swr'
 
 const theme = createTheme({
   palette: {
@@ -14,28 +14,22 @@ const theme = createTheme({
   },
 })
 
+const swrConfig = {
+  dedupingInterval: 3000,
+  errorRetryCount: 3,
+  revalidateIfStale: false,
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false,
+}
+
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <OptionsProvider>
-        <Global
-          styles={css`
-            html {
-              font-family: 'Noto Sans KR';
-            }
-          `}
-        />
-        <Table />
+        <SWRConfig value={swrConfig}>
+          <Table />
+        </SWRConfig>
       </OptionsProvider>
     </ThemeProvider>
   )
 }
-
-// function uniqueID() {
-// 	return Math.floor(Math.random() * Date.now())
-// };
-//  const wrapperId = `table-${uniqueID()}`
-//  console.log('@@wrapperId',wrapperId)
-//   const wrapper = document.createElement('div');
-// wrapper.setAttribute('id',wrapperId);
-//   instance.canvas.append(wrapper);
