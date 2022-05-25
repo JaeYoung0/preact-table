197cb1fa2f02fcae1fcd79eb5d6db52df86e76be
JaeYoung0 <hjy8697@gmail.com>
2022-05-25 21:29:59 +0900

    fix: extractXLSX * sheetData에서 id, account_id 프로퍼티 제거

--- src/helper/extractXLSX.ts
+++ src/helper/extractXLSX.ts
@@ -1,9 +1,18 @@
+import { RowType } from '@/hooks/useMetrics'
 import * as XLSX from 'xlsx'
 
-export default function extractXLSX(filename: string, rows: any[]) {
-  filename = `${filename}.xlsx`
-  const sheet_data = rows
-  const ws = XLSX.utils.json_to_sheet(sheet_data)
+export default function extractXLSX(name: string, rows: RowType[]) {
+  const filename = `${name}.xlsx`
+  const sheetData = rows.map((row) => {
+    if (row.id || row.id === 0) {
+      delete row.id
+    } else if (row.account_id || row.account_id === 0) {
+      delete row.account_id
+    }
+    return row
+  })
+
+  const ws = XLSX.utils.json_to_sheet(sheetData)
   const wb = XLSX.utils.book_new()
   XLSX.utils.book_append_sheet(wb, ws, '실적데이터')
   XLSX.writeFile(wb, filename)
